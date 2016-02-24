511fcd82f23d469d0a2706551aed6bb9eb127adf
Porter <porter@Porters-MacBook-Air.local>
2016-02-23 20:31:55 -0800

    Videos first addition

--- js/main.js
+++ js/main.js
@@ -12,16 +12,13 @@ app.config([ '$routeProvider', function($routeProvider) {
 	.when("/", {
 		templateUrl : "partials/introduccion.html",
 		controller : "PageCtrl"
-	})
-	.when("/capitulo1", {
+	}).when("/capitulo1", {
 		templateUrl : "partials/capitulo1.html",
 		controller : "MainCtrl"
-	})
-	.when("/agradecimientos", {
+	}).when("/agradecimientos", {
 		templateUrl : "partials/agradecimientos.html",
 		controller : "PageCtrl"
-	})
-	.when("/contacto", {
+	}).when("/contacto", {
 		templateUrl : "partials/contacto.html",
 		controller : "PageCtrl"
 	})
@@ -32,22 +29,23 @@ app.config([ '$routeProvider', function($routeProvider) {
 	});
 } ]);
 
+
 /**
  * Controls all other Pages
  */
 app.controller('PageCtrl', function($scope, $http) {
-	//DO nothing for now, placeholder
+	// DO nothing for now, placeholder
 });
 
 app.controller('MainCtrl', function($scope, $http) {
-	window.scrollTo(0,0);
+	window.scrollTo(0, 0);
 	$('.bio').hide();
 	$('.bio-button').unbind('click');
-	
+
 	$('.bio-button').click(function() {
 		$('.bio').slideToggle(500);
 		$('.bio-button').slideToggle(200, function() {
-			if($('.bio-button span').html() === 'Bio') {
+			if ($('.bio-button span').html() === 'Bio') {
 				$('.bio-button span').html('Hide');
 			} else {
 				$('.bio-button span').html('Bio');
@@ -55,4 +53,6 @@ app.controller('MainCtrl', function($scope, $http) {
 			$('.bio-button').slideToggle(200);
 		});
 	});
+
 });
+
