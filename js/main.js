6c1a24cad732fdaf2a3f696c55077e7a8cd30fe8
Porter <porter@Porters-MacBook-Air.local>
2016-02-25 06:36:12 -0800

    Added twitter in contacts + Bio image slider

--- js/main.js
+++ js/main.js
@@ -55,6 +55,7 @@ app.controller('MainCtrl', function($scope, $http) {
 	});
 	
 	startVideoEvents();
+	setInterval(cycleImages, 5000);
 });
 
 function startVideoEvents() {
@@ -103,4 +104,14 @@ function loadPlayer(path) {
 	});
 	$('.close-player').show();
 }
+
+function cycleImages(){
+    var $active = $('#cycler .active');
+    var $next = ($active.next().length > 0) ? $active.next() : $('#cycler img:first');
+    $next.css('z-index',2);//move the next image up the pile
+    $active.fadeOut(1500,function(){//fade out the top image
+	      $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+        $next.css('z-index',3).addClass('active');//make the next image the top one
+    });
+  }
 
