6afad706a2e4b904ea2bf42943864f04affed24f
Porter <porter@Porters-MacBook-Air.local>
2016-08-29 19:46:47 -0300

    Added new video link

--- js/i18n.js
+++ js/i18n.js
@@ -6,27 +6,27 @@ app.config(function($translateProvider) {
 	$translateProvider.translations('es', {
 
 		intro_title : 'Introducción',
-		
+
 		intro_begin : 'Leer en voz alta, como si se entendiera',
-		
+
 		intro_l1 : 'T',
 		intro_p1 : 'ODO es posible”; aforismo menos popular que su pesimista contraparte "Nada es imposible", todo es posible nos dice que todo aquello que se encuentra dentro del universo de lo posible, de la posibilidad: puede ocurrir. Teniendo en cuenta que sólo lo posible ocurre, valga la redundancia (lo imposible es lo que no ocurre), y sólo existe lo que ocurre, entonces, “TODO puede ocurrir". Puede que a primera vista resulte poco, pero si uno lo piensa se dará cuenta de que hay infinitas posibilidades dentro de \'lo posible\', depende de uno recorrerlas o no, y, si TODO este infinito es posible de ocurrir, agrego, también está lo posible de hacer.',
-	
+
 		intro_l2 : 'T',
 		intro_p2 : 'ODO puede hacerse" sería un aforismo hermano, ya que sólo lo posible de ocurrir, de existir, es lo que puede hacerse. Uno hace lo posible, ni más ni menos. “TODO es posible” igual “Lo POSIBLE es todo” escrito en tiempo presente, contiene potencial en un tiempo futuro.',
-	
+
 		intro_l3 : 'E',
 		intro_p3 : 's decisión nuestra como nos paramos frente a esta afirmación muy poderosa dentro del lenguaje, una de la cual podemos echar mano, si tenemos los huevos suficientes... (u ovarios)',
-	
+
 		intro_end : 'Continuar',
 //		INTRO END
-		
+
 //		CHAPTER 1 BEGIN
 		ch1_title1 : 'Capítulo',
 		ch1_title2 : 'Prevención de femicidio',
-		
+
 		decalogo_title : 'Decálogo',
-		
+
 		decalogo_0 : 'Decálogo del aprendiz de la escucha',
 		decalogo_1 : 'Lo máximo máximo máximo que puedo cambiar es a uno mismo.',
 		decalogo_2 : 'Lo máximo máximo máximo que puedo dar a otro, es amor.',
@@ -38,9 +38,9 @@ app.config(function($translateProvider) {
 		decalogo_8 : 'No juzgues si no quieres juzgarte a ti mismo.',
 		decalogo_9 : 'Todo lo que Yo posea, me termina poseyendo.',
 		decalogo_10 : 'Si no te estás divirtiendo, algo estás haciendo mal.',
-		
+
 		bio_title : 'Bio',
-		
+
 		bio_1 : 'El licenciado en psicología Francisco J. Piagentini (Santa Cruz, Argentina, 1985) trabajó de los 21 a los 29 años como voluntario en el Centro de Asistencia al Suicida de Buenos Aires, cinco años al teléfono y luego como miembro de la comisión directiva. Presentó trabajos en varios congresos nacionales y regionales sobre prevención de suicidio y fue miembro del equipo profesional de capacitación para aspirantes a voluntarios.',
 		bio_2 : 'A su vez ejerce su práctica profesional desde hace algunos años como psicólogo clínico en su consultorio de Parque Centenario.',
 		bio_3 : 'Con una fuerte base psicoanalítica, fruto de posgrados en Centro Dos y la mitad de una maestría en ICdeBA, Francisco suma el paradigma cuántico y sabidurías orientales y americanas en su trabajo profesional diario.',
@@ -48,9 +48,11 @@ app.config(function($translateProvider) {
 		bio_5 : 'Actualmente es profesor del Bachiller de educación popular Independencia y trabaja como "psicologo de comercios" en Petit Morrison Café y Pizzería Rio.',
 		bio_6 : 'De espíritu inquieto y lúdico, está desarrollando este proyecto, un Libro-Web vivo con intención de generar un impacto a nivel social a través de las redes sociales.',
 		bio_7 : 'La escucha que habla es tanto un libro como una técnica de escucha, que el autor, bajo el alias de el aprendiz de la escucha, va descubriendo a medida que hace la obra.',
-		
+
+		more_video_link : 'Ver nuevo video',
+
 		thanks : 'Agradecimientos',
-		
+
 		thanks_0_0 : 'En primer lugar a',
 		thanks_0_1 : ', camarógrafa, por la buena disposición para filmar.',
 		thanks_1_0 : 'Gracias a mi hermano, el ingeniero',
@@ -66,38 +68,38 @@ app.config(function($translateProvider) {
 		thanks_10_0 : 'Gracias a',
 		thanks_10_1 : ', y Daniel, por prestar el lugar en Sarmiento 4555, invitar los cafés y la buena onda.',
 		thanks_11 : 'Gracias a Pizzería Río, y Néstor, por prestar el lugar y la sabiduría (y las pizzas) en Rio de Janeiro 863.',
-		
+
 		contact : 'Contacto',
 //		CHAPTER 1 END
-		
+
 		back : 'Volver',
-		
+
 		descargas : 'Descargas',
 		descarga_link : 'Descargar',
 	}).translations('en', {
 //		INTRO BEGIN
 		intro_title : 'Introduction',
-		
+
 		intro_begin : 'Read out loud, as if understanding',
-		
+
 		intro_l1 : 'E',
 		intro_p1 : 'VERYTHING is possible; less popular aphorism than its counterpart "Nothing is impossible", everything is possible tells us that everything in the universe of what is possible, of what is a possibility: can happen. Considering that only the possible can happen, despite the redundancy (the impossible is what doesn\'t happen), and only what happens exists, then, "EVERYTHING can happen". It might seem little at first sight, but if one thinks about it there are infinite possibilities within \'what is possible\', depends on whether to go through them or not, and, if this EVERYTHING can happen, I add, then also there are the things possible to do.',
-		
+
 		intro_l2 : 'E',
 		intro_p2 : 'VERYTHING can be done" would be a brother aphorism, given only what is possible of happening, if existing, can be done. One does what is possible, no more no less. "EVERYTHING is possible" the same "What is POSSIBLE is everything" written in present tense, contains the potential of a future tense.',
-	
+
 		intro_l3 : 'I',
 		intro_p3 : 't is our decision how we stand in front of this so powerful affirmation within language, which we can make use of, if we have big enough balls... (or ovaries)',
-	
+
 		intro_end : 'Continue',
 //		INTRO END
-		
+
 //		CHAPTER 1 BEGIN
 		ch1_title1 : 'Chapter',
 		ch1_title2 : 'Femicide prevention',
-		
+
 		decalogo_title : 'Decalogue',
-		
+
 		decalogo_0 : 'Decalogue of the aprendiz de la escucha',
 		decalogo_1 : 'The maximum maximum maximum one can change is themselves.',
 		decalogo_2 : 'The maximum maximum maximum one can give another one, is love.',
@@ -109,9 +111,9 @@ app.config(function($translateProvider) {
 		decalogo_8 : 'Do not judge if you don\'t want to be judged yourself.',
 		decalogo_9 : 'Everything I possess, ends up possessing me.',
 		decalogo_10 : 'If you are not having fun, you are not doing it right.',
-		
+
 		bio_title : 'Bio',
-		
+
 		bio_1 : 'Psychology graduate Francisco J. Piagentini (Santa Cruz, Argentina, 1985) worked from being 21 to 29 years old as a voluntary in the Buenos Aires Suicide Assistance Center, five year on the phone and then as a member of the directive commission. Presented several projects on suicide prevention in national and regional congresses and he was member of a professional training aimed for volunteers and aspirings.',
 		bio_2 : 'Performs also in his proffessional practice as a clinical psychologist since a few yeas back in his office near Parque Centenario',
 		bio_3 : 'With a strong psychoanalytical foundation, result of a postgraduate in Centro Dos and half a masters in ICdeBA, Francisco sums the quantic paradigm, oriental and american wisdom to his daily professional work.',
@@ -119,9 +121,11 @@ app.config(function($translateProvider) {
 		bio_5 : 'Currently he is a teacher in the Independencia Bachelor of popular education and works as a "commercial store psychologist" at Petit Morrison Café and Pizzería Rio.',
 		bio_6 : 'Of restless and playful spirit, he is developing this project, a Book-Web alive with the intention of generation a social impact through the use of social networks.',
 		bio_7 : 'La escucha que habla is so much a book as it is a listening technique, which the author, under the alias of aprendiz de la escucha, keeps discovering as he creates the work.',
-		
+
+		more_video_link : 'Watch new video',
+
 		thanks : 'Thanks',
-		
+
 		thanks_0_0 : 'In first place to',
 		thanks_0_1 : ', photographer, for the good disposition for filming.',
 		thanks_1_0 : 'Thanks to my brother, engineer',
@@ -137,12 +141,12 @@ app.config(function($translateProvider) {
 		thanks_10_0 : 'Thanks to',
 		thanks_10_1 : ', and Daniel, for lending the place in Sarmiento 4555, inviting the coffees and the good vibe.',
 		thanks_11 : 'Thanks to Pizzería Río, and Néstor, for lending the place and the wisdom (and the pizzas) at Rio de Janeiro 863.',
-		
+
 		contact : 'Contact',
 //		CHAPTER 1 END
-		
+
 		back : 'Back',
-		
+
 //		DOWNLOADS
 		descargas : 'Dowloads',
 		descarga_link : 'Download',
